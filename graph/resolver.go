b548c41baab1934ea40545ec13a1f8208a987e02
syunmaro <haruku1313@gmail.com>
2023-12-24 15:15:23 +0900

    first commit

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import (
+	"graphql-test-api/ent"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{ Client *ent.Client }
